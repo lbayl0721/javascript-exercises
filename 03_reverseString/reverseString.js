ad723c60b44bb172063a9ab9bb9d9beb35f1c4df
Luke Bayless <baylessluke@gmail.com>
2023-03-27 20:53:41 -0400

    completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    /*find the length of the string the for loop to iterate building string from last to first*/
+    return phrase.split("").reverse().join("");
 };
 
 // Do not edit below this line
