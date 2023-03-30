fa8555a57ef86b3874966971728e2d5d5ca315bb
Luke Bayless <baylessluke@gmail.com>
2023-03-29 20:26:52 -0400

    Completed exercise 08

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...test) {
+/*filter through for loop, if remove = the item, then delete*/
+    const newArray = [];
+    array.forEach((item) => {
+        if (!test.includes(item)) {
+            newArray.push(item);
+        }
+    })
+    return newArray;
 };
 
 // Do not edit below this line
